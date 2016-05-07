cc53d51faf18a625173f12d2e39fc49ab0186779
Michael Verges <maustinstar@gmail.com>
2016-05-07 12:35:43 -0500

    Universal Constants

---
+++ scholarsNearMe/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  scholarsNearMe
+//
+//  Created by Michael Verges on 5/7/16.
+//
+
+import Foundation
+import UIKit
+
+// MARK: Screen Dimensions
+
+let screenSize = UIScreen.mainScreen().bounds
+let screenWidth = screenSize.width      //use for device compatibility
+let screenHeight = screenSize.height    //use for device compatibility
+                                        //e.g. CGPointMake(screenWidth*0.1, screenHeight*0.7)
+
+// MARK: User Interface Colors
+let colorMain = UIColor(red:0.32, green:0.65, blue:0.98, alpha:1.0)
+let colorSecondary = UIColor(red:0.01, green:0.40, blue:0.75, alpha:1.0)
+let colorDark = UIColor(red:0.02, green:0.15, blue:0.34, alpha:1.0)
+let colorBackground = UIColor .whiteColor()
